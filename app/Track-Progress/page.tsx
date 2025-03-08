9f68da107c95ffe795c5ed5c7ca472c79b9d5000
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-08 12:20:56 +0530

    Fix formatting in Track Progress page component

--- app/Track-Progress/page.tsx
+++ app/Track-Progress/page.tsx
@@ -1,8 +1,10 @@
 import NavbarMain from "@/components/Navbar";
 
 export default function Leaderboard() {
-    return (<div>
+    return (
+    <div>
         <NavbarMain />
         <h1 className="text-center text-4xl pt-16">Progress Analytics will appear here</h1>
-    </div>);
+    </div>
+    );
 }
