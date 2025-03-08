e49233a75ba27fc87e045b2e7d9ba389043a1859
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-08 11:30:05 +0530

    Add Navbar component to BackgroundBeamsDemo for improved navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,37 @@
 "use client";
+import { AcmeLogo } from "@/components/Navbar";
 import { BackgroundBeams } from "@/components/ui/background-beams";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle } from "@heroui/navbar";
+import { Button } from "@heroui/react";
+import Link from "next/link";
 import React from "react";
 
 export default function BackgroundBeamsDemo() {
   return (
+    <div>
+    <Navbar isBordered>
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle aria-label="Open menu" />
+      </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center"> 
+      <NavbarBrand>
+          <AcmeLogo />
+          <Link className="font-bold text-inherit" href="/Dashboard">SpyderWeb</Link>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="/Dashboard">Get Started</Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      </Navbar>
+      
     <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-          Welcome to SpyderWeb
+          SpyderWeb
         </h1>
         <p></p>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-md text-center relative z-10">
@@ -15,6 +39,7 @@ export default function BackgroundBeamsDemo() {
         </p>
       </div>
       <BackgroundBeams />
+    </div>
     </div>
   );
 }
