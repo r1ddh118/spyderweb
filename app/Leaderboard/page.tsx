74828fe8e277aae407d3ed51b1463cd3b357df02
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-08 11:44:36 +0530

    Update Leaderboard and Quiz components with Navbar and placeholder text

--- app/Leaderboard/page.tsx
+++ app/Leaderboard/page.tsx
@@ -3,6 +3,6 @@ import NavbarMain from "@/components/Navbar";
 export default function Leaderboard() {
     return (<div>
         <NavbarMain />
-        <h1 className="text-center text-4xl">Leaderboard will appear here</h1>
+        <h1 className="text-center text-4xl pt-16">Leaderboard will appear here</h1>
     </div>);
 }
---
+++ app/Quiz/page.tsx
@@ -0,0 +1,8 @@
+import NavbarMain from "@/components/Navbar";
+
+export default function Leaderboard() {
+    return (<div>
+        <NavbarMain />
+        <h1 className="text-center text-4xl pt-16">Quiz Interface will appear here</h1>
+    </div>);
+}
