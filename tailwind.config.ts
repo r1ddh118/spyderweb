a72091e2cdf0837958958b8e4601afd405f1cf4e
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-08 10:14:57 +0530

    Update Tailwind CSS configuration to include @heroui/theme and additional content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,22 @@
+import {heroui} from '@heroui/theme';
 const {
     default: flattenColorPalette,
   } = require("tailwindcss/lib/util/flattenColorPalette");
   
   /** @type {import('tailwindcss').Config} */
   module.exports = {
-    content: [
-      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  
-      // Or if using `src` directory:
-      "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "// Or if using `src` directory:\n      \"./src/**/*.{js,ts,jsx,tsx,mdx}\"",
+    "./node_modules/@heroui/theme/dist/components/navbar.js"
+  ],
     darkMode: "class",
     theme: {
       extend: {},
     },
-    plugins: [addVariablesForColors],
+  plugins: [addVariablesForColors,heroui()],
   };
   
   // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
