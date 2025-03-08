046bd9ad7f83c1ffadf0373ffbd551a740eeb0f7
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-08 10:24:20 +0530

    Integrate NavbarMain component into BentoGridDemo for improved layout

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,4 +1,6 @@
+"use client"
 import { cn } from "@/lib/utils";
+
 import React from "react";
 import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
 import {
@@ -10,23 +12,9 @@ import {
   IconSignature,
   IconTableColumn,
 } from "@tabler/icons-react";
+import NavbarMain from "@/components/Navbar";
 
-export default function BentoGridDemo() {
-  return (
-    <BentoGrid className="max-w-4xl mx-auto">
-      {items.map((item, i) => (
-        <BentoGridItem
-          key={i}
-          title={item.title}
-          description={item.description}
-          header={item.header}
-          icon={item.icon}
-          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
-        />
-      ))}
-    </BentoGrid>
-  );
-}
+
 const Skeleton = () => (
   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
 );
@@ -75,3 +63,23 @@ const items = [
     icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
   },
 ];
+
+export default function BentoGridDemo() {
+  return (
+    <div>
+      <NavbarMain/>
+    <BentoGrid className="max-w-4xl mx-auto">
+      {items.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          description={item.description}
+          header={item.header}
+          icon={item.icon}
+          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+        />
+      ))}
+    </BentoGrid>
+    </div>
+  );
+}
